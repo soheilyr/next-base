1476d760e8c6c353b639c1005358ae667ab45af5
soheilyr <s.yazdanyar.sy@gmail.com>
2024-11-03 12:52:41 +0330

    first commit

---
+++ components/Common/Button/Button.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+};
+
+const Template = () => <Button>Hiiiiiiiiiiiiiiii</Button>;
+
+export const Default = Template.bind({});
+export const Primary = Template.bind({});
+export const Secondary = Template.bind({});
+export const disabled = Template.bind({});
---
+++ components/Common/Button/Button.tsx
@@ -0,0 +1,19 @@
+// components/Button.tsx
+import React from 'react';
+
+interface ButtonProps {
+  onClick?: () => void;
+  children: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({ children }) => {
+  return (
+    <button className='w-[200px] rounded-md bg-blue-600 px-4 py-2 text-white focus:outline-none'>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
